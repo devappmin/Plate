7acddf0fc7fa86c600fe683294e4041a97137285
Tak Ji Won <jiwontak@soongsil.ac.kr>
2020-11-18 02:12:50 +0900

    Change LoginActivity's onClick Events and Create RegisterSelectActivity
    1. Change LoginActivity's onClick Events
    - Now login feature working with Firebase Auth.
    - test account : test@test.com / 12345678.
    - Make snackbar when input field is empty or wrong email, pw input.
    - Soft keyboard hides automatically when button pressed.
    - Now signup button works (it calls RegisterSelectActivity).
    
    2. Create RegisterSelectActivity
    - New component added on Manifest!

---
+++ app/src/main/java/com/petabyte/plate/ui/activity/RegisterSelectActivity.java
@@ -0,0 +1,16 @@
+package com.petabyte.plate.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.petabyte.plate.R;
+
+public class RegisterSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_select);
+    }
+}
