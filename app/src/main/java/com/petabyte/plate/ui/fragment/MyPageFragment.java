dd69d672460b08b2b9e8ee8da8ea53107b4cfca9
Tak Ji Won <jiwontak@soongsil.ac.kr>
2020-11-21 03:46:09 +0900

    Add a logout button to use temporarily on MyPageFragment

--- app/src/main/java/com/petabyte/plate/ui/fragment/MyPageFragment.java
+++ app/src/main/java/com/petabyte/plate/ui/fragment/MyPageFragment.java
@@ -1,15 +1,21 @@
 package com.petabyte.plate.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.petabyte.plate.R;
+import com.petabyte.plate.ui.activity.LoginActivity;
+import com.petabyte.plate.ui.activity.MainActivity;
+import com.petabyte.plate.ui.activity.RegisterActivity;
 
 public class MyPageFragment extends Fragment {
 
@@ -18,6 +24,18 @@ public class MyPageFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_mypage, container, false);
+
+        Button logout = v.findViewById(R.id.btn_logout);
+        logout.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
 
         return v;
     }
