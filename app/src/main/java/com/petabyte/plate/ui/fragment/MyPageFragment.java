6b51aab69fae57683c71f33bb837c02e136f749f
Tak Ji Won <jiwontak@soongsil.ac.kr>
2020-11-23 22:47:20 +0900

    Change MypageFragment's Layout and Change some minor things
    1. Change MypageFragment's Layout
    - First commit on MyPageFragment branch!
    - Add collapsing toolbar and nested scroll view to show all menu.
    - Logout feature is working.
    
    2. Change RegisterInfoActivity's text size measurement from dp to sp

--- app/src/main/java/com/petabyte/plate/ui/fragment/MyPageFragment.java
+++ app/src/main/java/com/petabyte/plate/ui/fragment/MyPageFragment.java
@@ -1,23 +1,37 @@
 package com.petabyte.plate.ui.fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.petabyte.plate.R;
 import com.petabyte.plate.ui.activity.LoginActivity;
 import com.petabyte.plate.ui.activity.MainActivity;
 import com.petabyte.plate.ui.activity.RegisterActivity;
 
+import org.w3c.dom.Text;
+
 public class MyPageFragment extends Fragment {
+
+    private TextView text_username;
+    private TextView text_usertype;
+    private TextView text_usermail;
+    private TextView btn_logout;
 
     @Nullable
     @Override
@@ -25,8 +39,11 @@ public class MyPageFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_mypage, container, false);
 
-        Button logout = v.findViewById(R.id.btn_logout);
-        logout.setOnClickListener(new View.OnClickListener(){
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)v.findViewById(R.id.collapsing_toolbar_mypage);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+
+        btn_logout = (TextView)v.findViewById(R.id.text_v_mypage_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
