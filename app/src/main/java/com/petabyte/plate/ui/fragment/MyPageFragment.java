91e2b6b419e0e1827f5240a34d4d314e0e59d027
Tak Ji Won <jiwontak@soongsil.ac.kr>
2020-11-26 03:00:48 +0900

    Change MyPageFragment's profile image feature, dialog, layout and Create some new resources
    1. MyPageFragment 프로필 이미지 기능 수정
    - 이미지 파일 명은 UID로 지정되며, Storage의 /user/MEMBER_TYPE/UID.png 와 같이 저장되어 있음.
    - Storage에서 이미지를 불러오는 작업의 편의를 위해 Glide 사용. (본 커밋에는 포함 x, HomeDevelopment branch의 것을 이용함)
    - DB에서 Profile/Image 의 값이 'DEFAULT'가 아니면 커스텀 이미지가 등록된 것으로 판단하고 Storage에서 해당 이미지를 불러옴.
    - 'DEFAULT'라면 이미지가 등록되어있지 않은것이므로 drawable/ic_character.xml 파일을 불러옴.
    - Glide로 이미지를 circleCrop 하고 image view에 설정.
    - 배경이 하얀색인 이미지일 경우 구분이 어려울 수 있어 약간의 border를 넣어줌.
    
    2. 추후 작업을 위한 준비
    - 프로필 이미지 변경 시 앨범을 호출하고 그에 대한 결과를 받아야 하므로 REQUEST_SELECT_IMAGE(==2020) 을 추가함
    - 권한 설정시 편의를 위해 TedPermission library 를 사용. (https://github.com/ParkSangGwon/TedPermission)
    - 이를 implement 하는 구문을 gradle dependency에 추가함.
    
    3. MyPageFragment 기타 수정
    - dialog의 title이 보이지 않도록 수정.
    - 소개문구 변경 dialog의 edit text에 기존 등록되어 있는 문구를 보여주도록 수정.
    - 기타 변수명 관련 issue 수정.
    
    4. RegisterActivity DB 내용 추가 관련 수정
    - 회원가입시 DB에 생성되는 내용 변경.
    - HOST 회원은 가입시 자동으로 기본 소개 문구가 지정됨. (Profile/Description : "안녕하세요. xxx입니다.")
    - 모든 회원은 가입시 기본 프로필 사진을 사용할 수 있도록 함. (Profile/Image : "DEFAULT")
    
    5. Resource 생성 및 추가.
    - 새로운 color 'colorLight' 추가. (#f9f8fa)
    - 프로필 이미지에 border 주기 위한 리소스 imageview_oval_border.xml 생성.
    - 기본 프로필 이미지로 사용될 ic_character.xml 생성.

--- app/src/main/java/com/petabyte/plate/ui/fragment/MyPageFragment.java
+++ app/src/main/java/com/petabyte/plate/ui/fragment/MyPageFragment.java
@@ -1,37 +1,106 @@
 package com.petabyte.plate.ui.fragment;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Point;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.Display;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.petabyte.plate.R;
 import com.petabyte.plate.ui.activity.LoginActivity;
-import com.petabyte.plate.ui.activity.MainActivity;
-import com.petabyte.plate.ui.activity.RegisterActivity;
-
-import org.w3c.dom.Text;
+import com.petabyte.plate.utils.ConnectionCodes;
+import com.petabyte.plate.utils.GlideApp;
 
 public class MyPageFragment extends Fragment {
+
+    public static final int SELECT_FROM_ALBUM = 1;
 
     private TextView text_username;
     private TextView text_usertype;
     private TextView text_usermail;
+    private ImageView image_userpics;
+
+    private TextView btn_check_reserve;
+    private TextView btn_add_dining;
+    private TextView btn_edit_desc;
     private TextView btn_logout;
+    private TextView btn_edit_pw;
+    private TextView btn_delete_acc;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+    private StorageReference storage;
+    private DatabaseReference ref_g, ref_h;
+    private String MEMBER_TYPE = "";
+    private String UID = "";
+
+    public StorageReference getStorageRef(String TYPE, String imageURI){
+        storage = FirebaseStorage.getInstance("gs://plate-f5144.appspot.com/")
+                .getReference("user").child(TYPE+"/"+imageURI);
+        Log.d("ji1dev", "getStorageRef >> uri = "+storage);
+        return storage;
+    }
+
+    public void setProfileImage(final String UID){
+        if(MEMBER_TYPE.equals("Host")){
+            ref_h.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String imageName = snapshot.child(UID).child("Profile/Image").getValue().toString();
+                    if(!imageName.equals("DEFAULT")){
+                        Log.d("ji1dev", "setProfileImage >> host");
+
+                        // set image with GlideApp
+                        GlideApp.with(getContext()).load(getStorageRef("host", imageName))
+                                .circleCrop()
+                                .into(image_userpics);
+
+                    }else{
+                        Log.d("ji1dev", "setProfileImage >> host, NO CUSTOM IMAGE UPLOADED!");
+                        GlideApp.with(getContext())
+                                .load(getResources().getDrawable(R.drawable.ic_character))
+                                .circleCrop()
+                                .into(image_userpics);
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {}
+            });
+        }else if(MEMBER_TYPE.equals("Guest")){
+            Log.d("ji1dev", "setProfileImage >> guest member");
+        }
+    }
 
     @Nullable
     @Override
@@ -42,18 +111,269 @@ public class MyPageFragment extends Fragment {
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)v.findViewById(R.id.collapsing_toolbar_mypage);
         collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
 
+        text_username = (TextView)v.findViewById(R.id.text_v_mypage_username);
+        text_usermail = (TextView)v.findViewById(R.id.text_v_mypage_usermail);
+        text_usertype = (TextView)v.findViewById(R.id.text_v_mypage_usertype);
+        image_userpics = (ImageView)v.findViewById(R.id.image_v_mypage_userimg);
+        btn_check_reserve = (TextView)v.findViewById(R.id.text_v_mypage_check_reservation);
+        btn_add_dining = (TextView)v.findViewById(R.id.text_v_mypage_add_dining);
+        btn_edit_desc = (TextView)v.findViewById(R.id.text_v_mypage_edit_description);
+        btn_edit_pw = (TextView)v.findViewById(R.id.text_v_mypage_edit_pw);
+        btn_delete_acc = (TextView)v.findViewById(R.id.text_v_mypage_delete_account);
         btn_logout = (TextView)v.findViewById(R.id.text_v_mypage_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener(){
 
+        // set using info card
+        btn_check_reserve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
-                getActivity().finish();
+                Log.d("ji1dev", "check reservation clicked");
+                //startActivity(new Intent(getActivity(), ReservationCheckActivity.class));
             }
         });
 
+        btn_add_dining.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ji1dev", "add dining clicked");
+                //startActivity(new Intent(getActivity(), AddDiningActivity.class));
+            }
+        });
+
+        // set account card
+        btn_edit_desc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeDialog("PROMPT", "EDIT_DESC", "소개문구 변경",
+                        "변경할 소개 문구를 입력해주세요.\n소개 문구는 즉시 반영돼요!", "간단한 문구 입력");
+            }
+        });
+
+        btn_edit_pw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeDialog("PROMPT", "EDIT_PW", "비밀번호 변경",
+                        "변경할 비밀번호를 입력해주세요.\n비밀번호를 바꾼 뒤에는 다시 로그인 해야돼요.", "여섯자리 이상 입력");
+            }
+        });
+
+        btn_delete_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeDialog("CONFIRM", "DEL_ACC", "회원 탈퇴",
+                        "정말 PLATE를 떠나시겠어요?", "");
+            }
+        });
+
+        btn_logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+        // set notification card
+
         return v;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // set user info card
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        ref_g = FirebaseDatabase.getInstance().getReference("User").child("Guest");
+        ref_h = FirebaseDatabase.getInstance().getReference("User").child("Host");
+        UID = user.getUid();
+
+        ref_g.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    Object g_uid = dataSnapshot.getKey();
+                    if(g_uid.equals(UID)){
+                        //Log.d("ji1dev", "[ref_g]key=" + dataSnapshot.getKey() + ", " + dataSnapshot.child("Profile/Name").getValue().toString());
+                        text_username.setText(dataSnapshot.child("Profile/Name").getValue().toString());
+                        text_usertype.setText("Guest 회원");
+                        text_usermail.setText(user.getEmail());
+                        MEMBER_TYPE = "Guest";
+                        setProfileImage(UID);
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+        ref_h.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    Object h_uid = dataSnapshot.getKey();
+                    if(h_uid.equals(UID)){
+                        //Log.d("ji1dev", "[ref_h]key=" + dataSnapshot.getKey() + ", " + dataSnapshot.child("Profile/Name").getValue().toString());
+                        text_username.setText(dataSnapshot.child("Profile/Name").getValue().toString());
+                        text_usertype.setText("Host 회원");
+                        text_usermail.setText(user.getEmail());
+                        MEMBER_TYPE = "Host";
+                        setProfileImage(UID);
+                        btn_add_dining.setVisibility(View.VISIBLE);
+                        btn_edit_desc.setVisibility(View.VISIBLE);
+                        if(dataSnapshot.child("Status").getValue().toString().equals("WAITING")){
+                            btn_add_dining.setTextColor(getResources().getColor(R.color.textDarkDisabled));
+                            btn_add_dining.setText("다이닝 일정 추가 [심사 진행중]");
+                            btn_add_dining.setEnabled(false);
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+    }
+
+    // logout function
+    public void logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish();
+    }
+
+    // calculate dimension
+    public static int dpToPx(float dp, Resources resources) {
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics());
+        return (int) px;
+    }
+
+    // get image from album(gallery app)
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, ConnectionCodes.REQUEST_SELECT_IMAGE);
+    }
+
+    // function for make dialog with TYPE and other PARAMS
+    // TYPE : PROMPT, CONFIRM
+    public AlertDialog.Builder makeDialog(String type, final String action, String title, String message, String extra){
+        AlertDialog.Builder d = new AlertDialog.Builder(getActivity());
+        //d.setTitle(title);
+        d.setMessage(message);
+
+        // make layout for set view's margin
+        LinearLayout container = new LinearLayout(getActivity());
+        container.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams
+                (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        int left_margin = this.dpToPx(20, getActivity().getResources());
+        int top_margin = this.dpToPx(0, getActivity().getResources());
+        int right_margin = this.dpToPx(20, getActivity().getResources());
+        int bottom_margin = this.dpToPx(0, getActivity().getResources());
+        lp.setMargins(left_margin, top_margin, right_margin, bottom_margin);
+
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("처리중...");
+        progressDialog.setCancelable(true);
+        progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Horizontal);
+
+        // PROMPT BOX TYPE
+        if(type.equals("PROMPT")){
+            final EditText input = new EditText(getActivity());
+            input.setLayoutParams(lp);
+            input.setGravity(android.view.Gravity.TOP | android.view.Gravity.LEFT);
+            input.setHint(extra);
+
+            if(MEMBER_TYPE.equals("Host") && action.equals("EDIT_DESC")){
+                ref_h.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                            Object h_uid = dataSnapshot.getKey();
+                            if(h_uid.equals(user.getUid())){
+                                input.setText(dataSnapshot.child("Profile/Description").getValue().toString());
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {}
+                });
+            }
+
+            container.addView(input, lp);
+            d.setView(container);
+            d.setPositiveButton("바꾸기", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // force hide keyboard on fragment
+                    InputMethodManager mInputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    mInputMethodManager.hideSoftInputFromWindow(input.getWindowToken(), 0);
+
+                    String new_input = input.getText().toString().trim();
+                    switch(action){
+                        case "EDIT_PW":
+                            if(new_input.length() < 6) return;
+                            progressDialog.show();
+                            user.updatePassword(new_input).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        progressDialog.dismiss();
+                                        logout();
+                                    }
+                                }
+                            });
+                        case "EDIT_DESC":
+                            ref_h.child(user.getUid()).child("Profile/Description").setValue(new_input);
+                    }
+                    dialog.dismiss();
+                }
+            });
+            d.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {dialog.dismiss();}
+            });
+        // CONFIRM BOX TYPE
+        }else if(type.equals("CONFIRM")){
+            d.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    progressDialog.show();
+
+                    // remove user info from Realtime DB
+                    switch(MEMBER_TYPE){
+                        case "Host":
+                            ref_h.child(user.getUid()).removeValue();
+                        case "Guest":
+                            ref_g.child(user.getUid()).removeValue();
+                    }
+
+                    // remove user info from Authentication
+                    user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                progressDialog.dismiss();
+                                logout();
+                            }
+                        }
+                    });
+                    dialog.dismiss();
+                }
+            });
+            d.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {dialog.dismiss();}
+            });
+        }
+        // set btn color
+        AlertDialog tmp = d.create();
+        tmp.show();
+        tmp.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+        tmp.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+        return d;
+    }
+
+
+
 }
