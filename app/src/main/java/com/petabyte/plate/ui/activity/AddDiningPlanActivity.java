3b6c6339b7576bca5bbeee38cec88245550bb645
Shin Yeong Chan <syc1488@naver.com>
2020-11-27 18:57:04 +0900

    init commit for AddDiningPlanActivity

---
+++ app/src/main/java/com/petabyte/plate/ui/activity/AddDiningPlanActivity.java
@@ -0,0 +1,18 @@
+package com.petabyte.plate.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.petabyte.plate.R;
+
+public class AddDiningPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dining_plan);
+
+
+    }
+}
