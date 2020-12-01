893bd4701318500d30ed56d3c4bdbb5667adffef
Kim Seung Hwan <dev.appmin@gmail.com>
2020-12-01 21:12:28 +0900

    Add more list into HomeFragment
    1. Added SALTY ChipTypeList
    
    2. Added SWEET ChipTypeList
    
    3. Added function about select random value in FoodStyle

--- app/src/main/java/com/petabyte/plate/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/petabyte/plate/ui/fragment/HomeFragment.java
@@ -24,6 +24,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.petabyte.plate.R;
+import com.petabyte.plate.data.FoodStyle;
 import com.petabyte.plate.data.HomeAwardsData;
 import com.petabyte.plate.data.HomeCardData;
 import com.petabyte.plate.data.ImageSlideData;
@@ -38,7 +39,9 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
 
     private CardView searchButton;
     private ImageView applyImage;
-    private HomeHorizontalList specialList;
+    private HomeHorizontalList recentList;
+    private HomeHorizontalList foodTypeList;
+    private HomeHorizontalList[] chipTypeList;
     private HomeAwardsList postList;
     private HomeAwardsList imageSlider;
 
@@ -56,15 +59,21 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         // Firebase 초기화
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        // chipTypeList 배열 초기화
+        chipTypeList = new HomeHorizontalList[2];
+
         // 뷰를 불러온다.
         searchButton = (CardView) v.findViewById(R.id.search_card_fm_home);
         applyImage = (ImageView)v.findViewById(R.id.apply_iv_fm_home);
-        specialList = (HomeHorizontalList)v.findViewById(R.id.special_hh_fm_home);
+        recentList = (HomeHorizontalList)v.findViewById(R.id.recent_hh_fm_home);
         postList = (HomeAwardsList)v.findViewById(R.id.awards_ha_fm_home);
         imageSlider = (HomeAwardsList)v.findViewById(R.id.image_slide_fm_home);
         swipeRefreshLayout = (SwipeRefreshLayout)v.findViewById(R.id.swipe_layout_fm_home);
         mainLayout = (LinearLayout) v.findViewById(R.id.main_layout_fm_home);
         scrollView = (NestedScrollView)v.findViewById(R.id.scroll_v_fm_home);
+        foodTypeList = (HomeHorizontalList)v.findViewById(R.id.food_type_hh_fm_home);
+        chipTypeList[0] = (HomeHorizontalList)v.findViewById(R.id.chip_type_hh_fm_home);
+        chipTypeList[1] = (HomeHorizontalList)v.findViewById(R.id.chip_type_2_hh_fm_home);
 
         // 스와이프 시 Listener 설정
         swipeRefreshLayout.setOnRefreshListener(this);
@@ -85,9 +94,12 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         imageSlider.setType(HomeAwardsList.TYPE_MODE.IMAGE_SLIDE_MODE);
         imageSlider.removePadding();
 
-        loadSpecialList();
+        loadRecentList();
         loadPlatePost();
         loadImageSlider();
+        loadChipList(FoodStyle.SWEET, chipTypeList[0]);
+        loadChipList(FoodStyle.SALTY, chipTypeList[1]);
+        loadFoodTypeList("한식 다이닝", foodTypeList);
 
         //region 지원하기 카드뷰 관련 코드
         Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/plate-f5144.appspot.com/o/chef.png?alt=media&token=2e0e6f43-2523-482e-82ff-f5cd5ad54e19")
@@ -96,15 +108,15 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         return v;
     }
 
-    private void loadSpecialList() {
-        specialList.setTitle("최근에 올라온 음식이에요.");
+    private void loadRecentList() {
+        recentList.setTitle("최근에 올라온 음식이에요.");
         mDatabase.child("Dining").limitToLast(5).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshots) {
                 for (DataSnapshot snapshot : snapshots.getChildren()) {
                     HomeCardData data = snapshot.getValue(HomeCardData.class);
                     data.setImageUri(snapshot.getKey() + "/" + snapshot.child("dishes/1").getValue(String.class) + ".jpg");
-                    specialList.addData(data);
+                    recentList.addData(data);
                 }
             }
 
@@ -151,13 +163,61 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         });
     }
 
+    private void loadChipList(final FoodStyle foodStyle, final HomeHorizontalList list) {
+        list.setTitle("#" + foodStyle.label + "은 어떤가요?");
+        mDatabase.child("Dining").limitToLast(5).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshots) {
+                for (DataSnapshot snapshot : snapshots.getChildren()) {
+                    if (snapshot.child("style").hasChild(foodStyle.toString())) {
+                        HomeCardData data = snapshot.getValue(HomeCardData.class);
+                        data.setImageUri(snapshot.getKey() + "/" + snapshot.child("dishes/1").getValue(String.class) + ".jpg");
+                        list.addData(data);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void loadFoodTypeList(final String type, final HomeHorizontalList list) {
+        list.setTitle(type + " 모음");
+        mDatabase.child("Dining").limitToLast(5).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshots) {
+                for (DataSnapshot snapshot : snapshots.getChildren()) {
+                    if (snapshot.child("subtitle").getValue(String.class).equals(type)) {
+                        HomeCardData data = snapshot.getValue(HomeCardData.class);
+                        data.setImageUri(snapshot.getKey() + "/" + snapshot.child("dishes/1").getValue(String.class) + ".jpg");
+                        list.addData(data);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     @Override
     public void onRefresh() {
-        specialList.removeAllData();
+        recentList.removeAllData();
         postList.removeAllData();
         imageSlider.removeAllData();
-        loadSpecialList();
+        chipTypeList[0].removeAllData();
+        chipTypeList[1].removeAllData();
+        foodTypeList.removeAllData();
+        loadRecentList();
         loadPlatePost();
         loadImageSlider();
+        loadChipList(FoodStyle.SWEET, chipTypeList[0]);
+        loadChipList(FoodStyle.SALTY, chipTypeList[1]);
+        loadFoodTypeList("한식 다이닝", foodTypeList);
     }
 }
